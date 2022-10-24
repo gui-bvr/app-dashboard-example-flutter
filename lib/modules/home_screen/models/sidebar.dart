286da4a82f932bde50ec3d02bc8f0c39f3b822e3
Guilherme Viana <guibvianaribeiro@gmail.com>
2022-10-24 00:34:11 -0300

    .

--- lib/modules/home_screen/models/sidebar.dart
+++ lib/modules/home_screen/models/sidebar.dart
@@ -12,135 +12,167 @@ class _SideBarState extends State<SideBar> {
   Widget build(BuildContext context) {
     return Sizer(
       builder: (context, orientation, deviceType) {
-        return Padding(
-          padding: const EdgeInsets.only(left: 16.0),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () => Scaffold.of(context).closeDrawer(),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Ionicons.grid_outline,
-                        color: secondaryGreyColor,
-                        size: 15.sp,
-                      ),
+        return Drawer(
+          backgroundColor: primaryGreyColor,
+          width: 70.w,
+          elevation: 10,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
                       Text(
-                        '  Dashboard',
+                        'Dashboard',
                         style: TextStyle(
-                          color: secondaryGreyColor,
-                          fontSize: 15.sp,
+                          color: primaryBackground,
+                          fontSize: 20.sp,
                           fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.w600,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(height: 10.h),
+                      GestureDetector(
+                        onTap: () => Scaffold.of(context).closeDrawer(),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Ionicons.grid_outline,
+                              color: secondaryGreyColor,
+                              size: 15.sp,
+                            ),
+                            Text(
+                              '  Dashboard',
+                              style: TextStyle(
+                                color: secondaryGreyColor,
+                                fontSize: 15.sp,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 3.h),
+                      GestureDetector(
+                        onTap: () {
+                          Scaffold.of(context).closeDrawer();
+                          Get.toNamed('/example');
+                        },
+                        child: Row(
+                          children: [
+                            Icon(
+                              Ionicons.extension_puzzle_outline,
+                              color: secondaryGreyColor,
+                              size: 15.sp,
+                            ),
+                            Text(
+                              '  Página 1',
+                              style: TextStyle(
+                                color: secondaryGreyColor,
+                                fontSize: 15.sp,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 3.h),
+                      GestureDetector(
+                        onTap: () {
+                          Scaffold.of(context).closeDrawer();
+                          Get.toNamed('/example');
+                        },
+                        child: Row(
+                          children: [
+                            Icon(
+                              Ionicons.extension_puzzle_outline,
+                              color: secondaryGreyColor,
+                              size: 15.sp,
+                            ),
+                            Text(
+                              '  Página 2',
+                              style: TextStyle(
+                                color: secondaryGreyColor,
+                                fontSize: 15.sp,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 3.h),
+                      GestureDetector(
+                        onTap: () {
+                          Scaffold.of(context).closeDrawer();
+                          Get.toNamed('/example');
+                        },
+                        child: Row(
+                          children: [
+                            Icon(
+                              Ionicons.extension_puzzle_outline,
+                              color: secondaryGreyColor,
+                              size: 15.sp,
+                            ),
+                            Text(
+                              '  Página 3',
+                              style: TextStyle(
+                                color: secondaryGreyColor,
+                                fontSize: 15.sp,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10.h),
+                      Padding(
+                        padding: EdgeInsets.only(right: 20.w),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              child: ElevatedButton(
+                                onPressed: () =>
+                                    AuthController.authInstance.signOut(),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: primaryBackground,
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 5.h,
+                                    vertical: 3.w,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Logout',
+                                  style: TextStyle(
+                                    color: primaryGreyColor,
+                                    fontSize: 15.sp,
+                                    fontFamily: 'Montserrat',
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
                 ),
-                SizedBox(height: 3.h),
-                GestureDetector(
-                  onTap: () {
-                    Scaffold.of(context).closeDrawer();
-                    Get.toNamed('/example');
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Ionicons.extension_puzzle_outline,
-                        color: secondaryGreyColor,
-                        size: 15.sp,
-                      ),
-                      Text(
-                        '  Página 1',
-                        style: TextStyle(
-                          color: secondaryGreyColor,
-                          fontSize: 15.sp,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 3.h),
-                GestureDetector(
-                  onTap: () {
-                    Scaffold.of(context).closeDrawer();
-                    Get.toNamed('/example');
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Ionicons.extension_puzzle_outline,
-                        color: secondaryGreyColor,
-                        size: 15.sp,
-                      ),
-                      Text(
-                        '  Página 2',
-                        style: TextStyle(
-                          color: secondaryGreyColor,
-                          fontSize: 15.sp,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 3.h),
-                GestureDetector(
-                  onTap: () {
-                    Scaffold.of(context).closeDrawer();
-                    Get.toNamed('/example');
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Ionicons.extension_puzzle_outline,
-                        color: secondaryGreyColor,
-                        size: 15.sp,
-                      ),
-                      Text(
-                        '  Página 3',
-                        style: TextStyle(
-                          color: secondaryGreyColor,
-                          fontSize: 15.sp,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 3.h),
-                GestureDetector(
-                  onTap: () => AuthController.authInstance.signOut(),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Ionicons.log_out_outline,
-                        color: secondaryGreyColor,
-                        size: 15.sp,
-                      ),
-                      Text(
-                        '  Logout',
-                        style: TextStyle(
-                          color: secondaryGreyColor,
-                          fontSize: 15.sp,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       },
--- lib/routes/app_imports.dart
+++ lib/routes/app_imports.dart
@@ -10,18 +10,19 @@ export 'package:ionicons/ionicons.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:page_transition/page_transition.dart';
 export 'package:google_nav_bar/google_nav_bar.dart';
+export 'package:fl_chart/fl_chart.dart';
 
 //Files
 export 'app_routes.dart';
+export '../routes/app_pages.dart';
 export '../../utils/colors.dart';
 export '../../utils/datetime.dart';
+export '../services/auth_controller.dart';
+export '../services/auth_constants.dart';
 export '../modules/login_screen/login_screen.dart';
 export '../modules/login_screen/register_screen.dart';
 export '../modules/login_screen/forgotten_screen.dart';
-export '../routes/app_pages.dart';
 export '../modules/home_screen/home_screen.dart';
-export '../services/auth_controller.dart';
-export '../services/auth_constants.dart';
 export '../modules/home_screen/models/header.dart';
 export '../modules/home_screen/models/sidebar.dart';
 export '../modules/home_screen/models/categories.dart';
